5e4a7003dd9fb201b1b9e8fde3168704307de4da
Inseok <inseok.ucla@gmail.com>
2019-04-08 16:44:42 -0700

    Created createUser controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,11 +2,35 @@ const { getMongoDatabase } = require('../connections/mongoConnection');
 
 const UserController = {
   getUserByEmail: async (req, res) => {
-    const { email } = req.params;
-    const db = getMongoDatabase();
-    const collection = db.collection('user');
-    const user = await collection.findOne({ email });
-    res.send(user);
+    try {
+      const { email } = req.params;
+      const db = getMongoDatabase();
+      const collection = db.collection('user');
+      const user = await collection.findOne({ email });
+      res.send(user);
+    } catch (error) {
+      console.error('getUserByEmail error: ', error);
+    }
+  },
+  createUser: async (req, res) => {
+    try {
+      const {
+        name,
+        email,
+        role,
+      } = req.body;
+  
+      const db = getMongoDatabase();
+      const collection = db.collection('user');
+      await collection.insertOne({
+        name,
+        email,
+        role,
+      });
+      res.send('createdUser');
+    } catch (error) {
+      console.error('createUser error: ', error);
+    }
   },
 };
 
